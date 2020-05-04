cc31ad331d793ab3128af3b581ed7b48ea2c8a0b
Divy Srivastava <dj.srivastava23@gmail,com>
2020-05-04 20:07:45 +0530

    feat(loading): add a cool loading screen lol

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ var app = express();
 var path = require('path')
 var processRoute = require('./routes/process');
 
-app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/speak', processRoute);
 
 app.get('/', (req, res, next) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
+  res.sendFile(path.join(__dirname, 'public/loading.html'));
 })
+app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.get('/welcome', (req, res, next) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+})
 app.get('/main', (req, res, next) => {
   res.sendFile(path.join(__dirname, 'public/main.html'));
 })
